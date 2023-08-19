a997feecc281fcdc803f7a64c67b5242bdef4bdf
pranto <prantochandrodas@gmail.com>
2023-08-19 11:12:34 +0600

    add data table of mui

---
+++ src/Components/SelectCheckbox2/SelectCheckbox2.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import {
+  Checkbox,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Collapse,
+} from '@mui/material';
+import { ExpandMore, ExpandLess } from '@mui/icons-material';
+
+interface Subdepartment {
+  id: number;
+  name: string;
+}
+
+interface Department {
+  id: number;
+  name: string;
+  subdepartments: Subdepartment[];
+}
+
+const departments: Department[] = [
+  {
+    id: 1,
+    name: 'customer_service',
+    subdepartments: [
+      { id: 1, name: 'Subdepartment 1.1' },
+      { id: 2, name: 'Subdepartment 1.2' },
+      // Add more subdepartments as needed
+    ],
+  },
+  {
+    id: 2,
+    name: 'Department 2',
+    subdepartments: [
+      { id: 3, name: 'Subdepartment 2.1' },
+      { id: 4, name: 'Subdepartment 2.2' },
+      // Add more subdepartments as needed
+    ],
+  },
+  // Add more departments as needed
+];
+
+const SelectCheckbox2: React.FC = () => {
+  const [selectedDepartments, setSelectedDepartments] = useState<number[]>([]);
+  const [selectedSubdepartments, setSelectedSubdepartments] = useState<number[]>([]);
+  const [expandedDepartments, setExpandedDepartments] = useState<number[]>([]);
+
+  const handleDepartmentToggle = (departmentId: number) => {
+    const newSelectedDepartments = selectedDepartments.includes(departmentId)
+      ? selectedDepartments.filter(id => id !== departmentId)
+      : [...selectedDepartments, departmentId];
+
+    setSelectedDepartments(newSelectedDepartments);
+
+    // If selecting a department, select all of its subdepartments
+    const selectedDepartment = departments.find(dep => dep.id === departmentId);
+    if (selectedDepartment) {
+      const subdepartmentIds = selectedDepartment.subdepartments.map(subdep => subdep.id);
+      setSelectedSubdepartments(prevSelectedSubdepartments =>
+        newSelectedDepartments.includes(departmentId)
+          ? [...prevSelectedSubdepartments, ...subdepartmentIds]
+          : prevSelectedSubdepartments.filter(id => !subdepartmentIds.includes(id))
+      );
+    }
+  };
+
+  const handleSubdepartmentToggle = (subdepartmentId: number, departmentId: number) => {
+    const newSelectedSubdepartments = selectedSubdepartments.includes(subdepartmentId)
+      ? selectedSubdepartments.filter(id => id !== subdepartmentId)
+      : [...selectedSubdepartments, subdepartmentId];
+
+    setSelectedSubdepartments(newSelectedSubdepartments);
+
+    // Check if all subdepartments of a department are selected
+    const selectedDepartment = departments.find(dep => dep.id === departmentId);
+    if (selectedDepartment) {
+      const subdepartmentIds = selectedDepartment.subdepartments.map(subdep => subdep.id);
+      if (subdepartmentIds.every(id => newSelectedSubdepartments.includes(id))) {
+        handleDepartmentToggle(departmentId);
+      }
+    }
+  };
+
+  const handleDepartmentCollapseToggle = (departmentId: number) => {
+    setExpandedDepartments(prevExpandedDepartments =>
+      prevExpandedDepartments.includes(departmentId)
+        ? prevExpandedDepartments.filter(id => id !== departmentId)
+        : [...prevExpandedDepartments, departmentId]
+    );
+  };
+
+  return (
+    <List>
+      {departments.map(department => (
+        <React.Fragment key={department.id}>
+          <ListItem button onClick={() => handleDepartmentCollapseToggle(department.id)}>
+            <ListItemIcon>
+              {expandedDepartments.includes(department.id) ? <ExpandLess /> : <ExpandMore />}
+            </ListItemIcon>
+            <ListItemText primary={department.name} />
+            <Checkbox
+              edge="end"
+              checked={selectedDepartments.includes(department.id)}
+              onChange={() => handleDepartmentToggle(department.id)}
+            />
+          </ListItem>
+          <Collapse in={expandedDepartments.includes(department.id)} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {department.subdepartments.map(subdepartment => (
+                <ListItem
+                  key={subdepartment.id}
+                  button
+                  sx={{ pl: 4 }}
+                  onClick={() => handleSubdepartmentToggle(subdepartment.id, department.id)}
+                >
+                  <ListItemText primary={subdepartment.name} />
+                  <Checkbox
+                    edge="end"
+                    checked={selectedSubdepartments.includes(subdepartment.id)}
+                    onChange={() => handleSubdepartmentToggle(subdepartment.id, department.id)}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Collapse>
+        </React.Fragment>
+      ))}
+    </List>
+  );
+};
+
+export default SelectCheckbox2;
--- src/Components/TableInfo/TableInfo.tsx
+++ src/Components/TableInfo/TableInfo.tsx
@@ -1,9 +1,89 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@mui/material";
+import SelectCheckbox from '../SelectCheckbox/SelectCheckbox';
+import SelectCheckbox2 from '../SelectCheckbox2/SelectCheckbox2';
 const TableInfo = () => {
+
+    const columns = [
+        { id: 'id', name: 'Id' },
+        { id: 'name', name: 'Title' },
+        { id: 'email', name: 'body' },
+    ]
+
+    const handlechangepage = (event: React.MouseEvent<HTMLButtonElement> | null, newpage: number) => {
+        pagechange(newpage)
+    }
+    const handleRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        rowperpagechange(+event.target.value)
+        pagechange(0);
+    }
+
+    const [rows, rowchange] = useState<any[]>([]);
+    const [page, pagechange] = useState<number>(0);
+    const [rowperpage, rowperpagechange] = useState<number>(5);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts").then(resp => {
+            return resp.json();
+        }).then(resp => {
+            rowchange(resp);
+            console.log(resp);
+        }).catch(e => {
+            console.log(e.message)
+        })
+
+    }, [])
+
+  rows.map(row=>console.log(row));
     return (
         <div>
-            <h1>HI</h1>
+            <div style={{ textAlign: 'center' }}>
+                <h1>Data Table</h1>
+
+                <Paper sx={{ width: '90%', marginLeft: '5%' }}>
+                    <TableContainer sx={{ maxHeight: 450 }}>
+                        <Table stickyHeader>
+                            <TableHead>
+                                <TableRow>
+                                    {columns.map((column) => (
+                                        <TableCell style={{ backgroundColor: 'black', color: 'white' }} key={column.id}>{column.name}</TableCell>
+                                    ))}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows && rows
+                                    .slice(page * rowperpage, page * rowperpage + rowperpage)
+                                    .map((row) => <TableRow key={row.id}>
+                                        <TableCell key={row.id}>
+                                            {row.id}
+                                        </TableCell>
+                                        <TableCell key={row.id}>
+                                            {row.body}
+                                        </TableCell>
+                                        <TableCell key={row.id}>
+                                            {row.title}
+                                        </TableCell>
+                                    </TableRow>)}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        rowsPerPage={rowperpage}
+                        page={page}
+                        count={rows.length}
+                        component="div"
+                        onPageChange={handlechangepage}
+                        onRowsPerPageChange={handleRowsPerPage}
+
+                    >
+
+                    </TablePagination>
+                </Paper>
+                
+            </div>
+
+            <SelectCheckbox></SelectCheckbox>
+            <SelectCheckbox2></SelectCheckbox2>
         </div>
     );
 };
